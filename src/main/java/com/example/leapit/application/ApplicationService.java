5706b3605e9509a252ca9d4cb20946100a289de8
hahamik <dlapdl0615@gmail.com>
2025-05-16 09:54:45 +0900

    개인 마이페이지 í지원현황 관리

--- src/main/java/com/example/leapit/application/ApplicationService.java
+++ src/main/java/com/example/leapit/application/ApplicationService.java
@@ -1,10 +1,30 @@
 package com.example.leapit.application;
 
+import com.example.leapit._core.error.ex.ExceptionApi404;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ApplicationService {
     private final ApplicationRepository applicationRepository;
+
+    // 개인 마이페이지 지원 현황 관리
+    public ApplicationResponse.MyPageDTO getMyApplication(Integer userId) {
+        if (userId == null) throw new ExceptionApi404("회원정보가 존재하지 않습니다.");
+
+        // 지원 현황 통계
+        ApplicationResponse.StatusDTO statusDto = applicationRepository.findSummaryByUserId(userId);
+        if (statusDto == null) {
+            statusDto = new ApplicationResponse.StatusDTO(0L, 0L, 0L);
+        }
+
+        // 지원 현황 목록 조회
+        List<ApplicationResponse.ItemDTO> applicationDTOs = applicationRepository.findApplicationsByUserId(userId);
+        // respDTO에 담기
+        ApplicationResponse.MyPageDTO respDTO = new ApplicationResponse.MyPageDTO(statusDto, applicationDTOs);
+        return respDTO;
+    }
 }
